ebcb1370caa2971a0c4afc4a6e658eb1c11c8d00
wusd <wsd5656126@foxmail.com>
2020-01-05 19:13:40 +0800

    增加lombok

--- 001st-spring-ioc/src/main/java/com/wusd/spring/bean/Student.java
+++ 001st-spring-ioc/src/main/java/com/wusd/spring/bean/Student.java
@@ -1,34 +1,31 @@
 package com.wusd.spring.bean;
 
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * @author wusd
  * @date 2020/1/3 16:27
  */
+@Data
+@ToString
 public class Student {
 	private String name;
 	private int age;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	@Override
-	public String toString() {
-		return "Student{" +
-				"name='" + name + '\'' +
-				", age=" + age +
-				'}';
-	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public int getAge() {
+//		return age;
+//	}
+//
+//	public void setAge(int age) {
+//		this.age = age;
+//	}
 }
